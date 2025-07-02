9e939b6c4502a8421e0c4d9b6e0ba046389c2553
Tony <41577315+LilMoke@users.noreply.github.com>
2025-07-02 11:42:16 -0400

    Initial Commit

---
+++ AudioApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  AudioApp
+//
+//  Created by Tony Scamurra on 7/2/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ AudioAppTests/AudioAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  AudioAppTests.swift
+//  AudioAppTests
+//
+//  Created by Tony Scamurra on 7/2/25.
+//
+
+import Testing
+
+struct AudioAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
